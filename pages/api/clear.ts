1bb6a11f333ced5413c109ca72b284a8bce7406c
bel1c10ud <mail@bellclou.de>
2022-10-18 13:51:18 +0900

    http header

--- pages/api/clear.ts
+++ pages/api/clear.ts
@@ -4,6 +4,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  res.setHeader('age', '0');
   res.setHeader('set-cookie', [
     `access_token=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict;`, 
     `expiry_timestamp=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict;`,
