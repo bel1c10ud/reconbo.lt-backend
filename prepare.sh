732f05533c6be17b38c4161fab0c49df91a9c706
bel1c10ud <mail@bellclou.de>
2023-07-22 15:50:40 +0900

    chore: prepare for change to public repo

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 git remote remove origin || echo 'Origin remote has already been removed.'
-git remote add dev https://github.com/bel1c10ud/reconbo.lt-backend-dev.git || echo 'Dev remote has already been added.'
 git remote add live https://github.com/bel1c10ud/reconbo.lt-backend.git || echo 'Live remote has already been added.'
