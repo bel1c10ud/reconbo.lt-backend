72e4c5d8bad17e7f89b6686c9a70d2272e39df6b
bel1c10ud <mail@bellclou.de>
2025-03-11 14:32:37 +0900

    chore: 헤드리스 브라우저 업데이트

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -36,7 +36,7 @@ export default async function handler(
             args: chromium.args,
             defaultViewport: chromium.defaultViewport,
             executablePath: await chromium.executablePath(
-              `https://${process.env.VERCEL_URL}/chromium-v131.0.1-pack.tar`
+              `https://${process.env.VERCEL_URL}/chromium-v133.0.0-pack.tar`
             ),
             headless: chromium.headless
           });
